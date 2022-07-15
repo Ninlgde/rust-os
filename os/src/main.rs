cd3342fd75fc1fef88dca5200cfdc3ed2b18c17c
ninlgde <ninlgde.ma@gmail.com>
2022-07-15 17:27:24 +0800

    hello world os

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(panic_info_message)]
+
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+mod log;
+
+#[cfg(feature = "board_qemu")]
+#[path = "boards/qemu.rs"]
+mod board;
+
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
+
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("\x1b[31mhello world\x1b[0m");
+    panic!("Shutdown machine!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
 }
